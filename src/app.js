66c4468e7a454200e351133535f196832193b87b
John Behan <behanj@gmail.com>
2019-07-23 16:01:46 +0100

    setup static resources

--- src/app.js
+++ src/app.js
@@ -1,18 +1,11 @@
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.get('', (req, res) => {
-	res.send('<h1>Weather</h1>');
-});
-
-app.get('/help', (req, res) => {
-	res.send('help page');
-});
-
-app.get('/about', (req, res) => {
-	res.send('<h1>about</h1>');
-});
+app.use(express.static(publicDirectoryPath));
 
 app.get('/weather', (req, res) => {
 	res.send({
