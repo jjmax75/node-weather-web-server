3e00c80c32743ac5375cf84cc45b242b3062bfa9
John Behan <behanj@gmail.com>
2019-07-23 16:23:15 +0100

    use handlebars

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+	res.render('index', {
+		title: 'Weather App',
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about', {
+		title: 'About page',
+	});
+});
+
+app.get('/Help', (req, res) => {
+	res.render('Help', {
+		title: 'Help page',
+	});
+});
 
 app.get('/weather', (req, res) => {
 	res.send({
